445e4776a16371a281bbc092c53a9b9e48e43a0f
Aleem365 <2681365@gamil.com>
2023-06-03 07:22:14 +0500

    created components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,60 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NoMatchPageComponent } from './no-match-page/no-match-page.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
+import { UpdateUserDetailComponent } from './update-user-detail/update-user-detail.component';
+import { UpdatePasswordComponent } from './update-password/update-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component:HomeComponent,
+    children: [
+      {
+        path:'user-login',
+        component:UserLoginComponent
+      },
+      {
+        path:'user-registration',
+        component:UserRegistrationComponent
+      },
+      {
+        path:'user-profile',
+        component:UserProfileComponent
+      },
+      {
+        path:'add-new-user',
+        component:AddUserComponent
+      },
+      {
+        path:'remove-user/:id',
+        component:DeleteUserComponent
+      },
+      {
+        path:'update-user-detail/:id',
+        component:UpdateUserDetailComponent
+      },
+      {
+        path:'update-passowrd/:id',
+        component:UpdatePasswordComponent
+      }
+    ]
+  },
+  {
+    path:'**',
+    component:NoMatchPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
+import { UpdateUserDetailComponent } from './update-user-detail/update-user-detail.component';
+import { UpdatePasswordComponent } from './update-password/update-password.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NoMatchPageComponent } from './no-match-page/no-match-page.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    DeleteUserComponent,
+    UpdateUserDetailComponent,
+    UpdatePasswordComponent,
+    RegistrationComponent,
+    UserRegistrationComponent,
+    UserLoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NoMatchPageComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
